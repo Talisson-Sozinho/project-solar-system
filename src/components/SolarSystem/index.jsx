04ca9677f9ff4744fafc92ffe2e1babbb5bdd4bd
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-05-10 00:51:13 -0300

    requisito 06 - componente "PlanetCard" renderizado com os planetas

--- src/components/SolarSystem/index.jsx
+++ src/components/SolarSystem/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PlanetCard from '../PlanetCard';
 import Title from '../Title';
+
+import Planets from '../../data/planets';
 
 export default class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          Planets.map(({ name, image }) => (
+            <PlanetCard
+              key={ name }
+              planetName={ name }
+              planetImage={ image }
+            />
+          ))
+        }
       </div>
     );
   }
